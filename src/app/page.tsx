b4093d9fa715af42609fb020d4f29af43f25921a
Lucas Maciel <lsukaplays@gmail.com>
2025-08-11 02:33:47 -0300

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import Image from "next/image";
 import { Header } from "@/components/common/header";
 import { db } from "@/db";
 import ProductList from "@/components/common/product-list";
+import CategorySelector from "@/components/common/category-selector";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: { variants: true },
   });
+
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -25,6 +28,9 @@ const Home = async () => {
         </div>
 
         <ProductList products={products} title="Mais vendidos" />
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
 
         <div className="px-5">
           <Image
